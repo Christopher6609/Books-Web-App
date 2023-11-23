0593709aa3b48a4a2f0608201b5094da0127adcb
Chris <odeniyichristopher7@gmail.com>
2023-11-23 17:17:28 +0000

    First books app commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {},
+  },
+  fontSize: {
+    'info': '0.5rem',
+  },
+  plugins: [],
+}
+
