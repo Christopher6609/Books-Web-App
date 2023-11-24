7f475538e754cd2f7912df71d8a9bf3fa54b3633
Intuneteq <tobiolanitori@gmail.com>
2023-11-24 10:23:55 +0000

    Helping Chris fix his code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'nav': "0px 4px 10px 0px rgba(0, 0, 0, 0.15)"
+      },
+    },
   },
   fontSize: {
     'info': '0.5rem',
