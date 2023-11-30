0cf70057ec65877c24096ce47c28928faa32d33e
Chris <odeniyichristopher7@gmail.com>
2023-11-30 23:36:22 +0000

    edited font and text size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,29 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
+    // screens: {
+    //   'phone': '480px',
+    //   // => @media (max-width: 480px){...}
+    // },
+
     extend: {
       boxShadow: {
         'nav': "0px 4px 10px 0px rgba(0, 0, 0, 0.15)"
       },
+
     },
+    fontFamily: {
+      manrope: ['Manrope']
+    },
+
+    fontFamily: {
+      merriweather: ['Merriweather']
+    },
+
+    fontFamily: {
+      sourcesans: ['Source Sans 3']
+    },
+
   },
   fontSize: {
     'info': '0.5rem',
